eb86c9bf233d87a9487f2c459ab07b1757ace92b
Kumani K <kumanikidd@gmail.com>
2025-03-25 08:00:40 +0000

    absolute path

--- src/app/components/KickerCardContainer.tsx
+++ src/app/components/KickerCardContainer.tsx
@@ -6,7 +6,7 @@ export default function KickerCardContainer() {
         <>
         <div className="flex-center">
             <h2 className="h2">Kicker Deck</h2>
-            <Image src='./images/arrow.svg' alt="South facing Arrow" width={35} height={35}/>
+            <Image src='/images/arrow.svg' alt="South facing Arrow" width={35} height={35}/>
         </div>
         <div className="grid">
             <KickerCard/>
@@ -17,7 +17,7 @@ export default function KickerCardContainer() {
 
         <div className="flex-center">
             <h2 className="h2">Completed Cards</h2>
-            <Image src='./images/arrow.svg' alt="South facing Arrow" width={35} height={35}/>
+            <Image src='/images/arrow.svg' alt="South facing Arrow" width={35} height={35}/>
         </div>
         <div className="grid">
             <KickerCard/>
